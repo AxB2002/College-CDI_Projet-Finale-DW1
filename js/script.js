502dfbf278405eeefe1d14705b79d7604f8ebf25
Adam Brillon <73497573+AxB2002@users.noreply.github.com>
2022-06-02 19:29:51 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+/* ========== typing animation ========== */
+var typed=new Typed(".typing",{
+    strings:["","astrology","horoscopes","zodiac signs"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
